9bde9a79d0dff266c02bfde3e78ee1f2588b0c20
Prabhav Bhatnagar <prabhav2b@gmail.com>
2017-09-07 21:29:09 +0530

    Update Backprop.py
    Apparently I uploaded the wrong file and lost the correct one, so I recreated it.

--- Backprop.py
+++ Backprop.py
@@ -1,81 +1,64 @@
 #Backpropogation implementaion program for XOR using numpy
 import numpy as np 
+import time
 
-#Packpropogation Neural Network with 1 hidden layer
-class nnXor:
+#Backpropogation Neural Network with 1 hidden layer
 
+#Activation Function used: ReLU
+def act(Squash_this):
+  return 1/(1 + np.exp(-Squash_this))
 
-	#Activation Function used: ReLU
-	#PH = Place Holder
-	def act(PH):
-		return PH*(PH>0)
+#Derivative of activation Function
+def act_prime(g):
+  return g*(1 - g)
 
-	#Derivative of activation Function
-	def act_prime(PH):
-		if PH>0:
-			return 1
-		else:
-			return 0
-
-
-	#Initialization
-	def __init__(self,alpha,Input_X,Input_Y):
-		self.a = alpha
-
-		self.x = np.array(Input_X) # 4 X 2
-		self.y = np.array(Input_X) # 4 X 1 
-
-		#Seeding(Ensures same random numbers)
-		np.random.seed(1)
-
-		#Random Initialization of Weights between -.5 to .5
-		self.W1 = np.random.random((4,5))-.5   # 4 X 5
-		self.W2 = np.random.random((1,5))-.5	  # 1 X 5
-
-		#Hidden Layer Initialization
-		self.Hidden = np.zeros((4,1))  # 4 X 1
-		self.output = np.zeros((4,1))  # 4 X 1
-
-		#Delta Initialization
-		self.Del_out = np.zeros((1,5))   # 1 X 5
-		self.Del_hid = np.zeros((4,5))  # 4 X 5
-
-
-
-	#Forward Pass
-	def Forward_Pass(self):
-		
-		self.Hidden = self.act(np.dot(self.W1,self.x))  # 4 X( 5 * 4 )X 2
-
-		self.output = self.act(np.dot(self.W2,self.Hidden)) # 1 X( 5 * 4 )X 2
-
-	#Back-Propogation
-	def Back_Prop(self):
-		
-		#The Error between the predicted and actual output.
-		self.Del_out = self.output-self.y 
-		self.Del_hid = np.dot(self.W2,self.Del_out)*act_prime(self.hidden)
-
-		#Parameter Updation
-		self.W2 = self.W2 - self.a*self.Del_out
-		self.W1 = self.W1 - self.a*self.Del_hid
+#Set Learning Rate Here
+a = 3
+#Seeding(Ensures same random numbers)
+np.random.seed(1)
 
 #Training set
-X = np.array([(0,0),(0,1),(1,0),(1,1)])											
-Y = np.array([0,1,1,0])
+X = np.array([[0,0],[0,1],[1,0],[1,1]])                     
+Y = np.array([[0],[1],[1],[0]])
 Iter = 6000
 
-#Object Created
-Exec = nnXor(.01,X,Y)
+#x = np.c_[np.ones(4), X] # 4 X 3
+#y = Y # 4 X 1 
+
+#Random Initialization of Weights between -.5 to .5
+W1 = 2*np.random.random((2,4)) - 1 
+W2 = 2*np.random.random((4,1)) - 1  
 
 #Clock Starts
 t_start = time.clock()
 
-#Training Iterations
-for no in range[Iter]:
-	Exec.Forward_Pass()
-	Exec.Back_Prop()
+
+for no in range(Iter):
+
+  #Forward Pass
+
+  hidden = act(np.dot(X,W1))
+  output = act(np.dot(hidden,W2))
+  err_out = Y-output
+
+  if (no%1000) == 0:
+    print "Error : " + str(np.mean(np.abs(err_out)))
+
+  #Back-Propogation
+
+  #The Error between the predicted and actual output.
+  del_out = err_out*act_prime(output)
+
+  err_hid = np.dot(del_out, W2.T)
+  del_hid = err_hid * act_prime(hidden)
+  
+  #Parameter Updation
+  W2 += a*np.dot(hidden.T,del_out)
+  W1 += a*np.dot(X.T, del_hid)
 
 #Time Stops here
 t_end = time.clock()
 print('Elapsed time ', t_end - t_start)
+
+print "Final Result: "
+print output
